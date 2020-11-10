57793cc504850c16c51096f2beca197f7518f759
cordenbrock <cordenbrock@gmail.com>
2020-11-10 13:56:16 -0800

    Initialize project
    co-authored-by: Nick Hennessey <njhnny@gmail.com>

---
+++ src/js/circle.js
@@ -0,0 +1,14 @@
+// export function Test1(test) {
+//   this.test = test;
+// }
+
+
+// (Circle.js)--Module = {
+//   Triangle: ... function
+//   Circle: ...function
+// }
+
+
+// import { Triangle, Circle } from  this File;
+
+
--- src/main.js
+++ src/main.js
@@ -4,24 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Triangle from './js/triangle.js';
 import Rectangle from './js/rectangle.js';
+// import { Test1 } from "./js/circle.js";
 
-$(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = parseInt($('#length1').val());
-    const length2 = parseInt($('#length2').val());
-    const length3 = parseInt($('#length3').val());
-    const triangle = new Triangle(length1, length2, length3);
-    const response = triangle.checkType();
-    $('#response').append(`<p>${response}</p>`);
-  });
+// let test = "test"
+// let testing = Test1(test);
+// console.log(testing);
 
-  $('#rectangle-area-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = parseInt($('#rect-length1').val());
-    const length2 = parseInt($('#rect-length2').val());
-    const rectangle = new Rectangle(length1, length2);
-    const response = rectangle.getArea();
-    $('#response2').append(`<p> The area of the rectangle is ${response}.</p>`);
-  });
+
+$('#triangle-checker-form').submit(function(event) {
+  event.preventDefault();
+  const length1 = parseInt($('#length1').val());
+  const length2 = parseInt($('#length2').val());
+  const length3 = parseInt($('#length3').val());
+  const triangle = new Triangle(length1, length2, length3);
+  const response = triangle.checkType();
+  $('#response').append(`<p>${response}</p>`);
+});
+
+$('#rectangle-area-form').submit(function(event) {
+  event.preventDefault();
+  const length1 = parseInt($('#rect-length1').val());
+  const length2 = parseInt($('#rect-length2').val());
+  const rectangle = new Rectangle(length1, length2);
+  const response = rectangle.getArea();
+  $('#response2').append(`<p> The area of the rectangle is ${response}.</p>`);
 });
